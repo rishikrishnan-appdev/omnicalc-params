7e45ffdc01af53fa29fe0e4b0cd3af86eee55d78
Rishi Krishnan <rishikrishnan@chicagobooth.edu>
2019-02-12 03:18:43 +0000

    done

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -75,4 +75,55 @@ class CalculationsController < ApplicationController
     )
   end
   
+  def square_new
+    render(:template => "calc_templates/square_new.html.erb")
+  end
+  
+  def square_results
+    
+    @number = params[:user_number].to_f
+    @square = @number ** 2
+    
+    render(:template => "calc_templates/square_results.html.erb")
+  end
+  
+  def root_new
+    render(:template => "calc_templates/root_new.html.erb")
+  end
+  
+  def root_results
+    
+    @number = params[:user_number].to_f
+    @root = @number ** 0.5
+    
+    render(:template => "calc_templates/root_results.html.erb")
+  end
+  
+  def payment
+    render(:template => "calc_templates/payment.html.erb")
+  end
+  
+  def payment_results
+    
+    @APR = params[:APR].to_f
+    @years = params[:years].to_f
+    @principal = params[:principal].to_f
+    
+    @monthly = (@APR / 1200 * @principal) / (1 - (1 + @APR / 1200) ** (-@years * 12))
+    
+    render(:template => "calc_templates/payment_results.html.erb")
+  end
+  
+  def random
+    render(:template => "calc_templates/random.html.erb")
+  end
+  
+  def random_results
+    
+    @small = params[:small].to_f
+    @big = params[:big].to_f
+    @random_result = rand(@small..@big)
+    render(:template => "calc_templates/random_results.html.erb")
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,22 @@ Rails.application.routes.draw do
   get("/flexible/square_root/:user_number", { :controller => "calculations", :action => "calc_root" })
   get("/flexible/payment/:bp/:years/:pv", { :controller => "calculations", :action => "monthly" })
   get("/flexible/random/:small/:big", { :controller => "calculations", :action => "calc_random" })
+  
+  get("/square/new", { :controller => "calculations", :action => "square_new" })
+  get("/square/results",  { :controller => "calculations", :action => "square_results" })
+  
+  get("/square_root/new", { :controller => "calculations", :action => "root_new" })
+  get("/square_root/results",  { :controller => "calculations", :action => "root_results" })
+  
+  get("/payment/new", { :controller => "calculations", :action => "payment" })
+  get("/payment/results",  { :controller => "calculations", :action => "payment_results" })  
+  
+  get("/random/new", { :controller => "calculations", :action => "random" })
+  get("/random/results",  { :controller => "calculations", :action => "random_results" }) 
+  
+  
+  get("/square_root/new", { :controller => "calculations", :action => "square_root_new" })
+  get("/payment/new", { :controller => "calculations", :action => "payment_new" })
+  get("/random/new", { :controller => "calculations", :action => "random_new" })
+
 end
